281904204745ed488b1d3e595786c1b5781a1d46
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2025-01-17 14:11:44 -0600

    fixing redirect

--- src/views/Splash.jsx
+++ src/views/Splash.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 const Index = () => {
+  const { isAuthenticated } = useAuth0();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log("logged in? -> ", isAuthenticated);
+    if (isAuthenticated) navigate("/profile");
+  });
+
   return (
     <div className="p-4 bg-white text-black">
       <Header />
